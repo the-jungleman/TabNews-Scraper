efe1ef26294204c3a6acc1495a750e7941c7eb81
jungleman <124931697+the-jungleman@users.noreply.github.com>
2023-11-24 10:11:24 -0300

    Add files via upload

--- projeto/main.py
+++ projeto/main.py
@@ -4,22 +4,21 @@ import  tkinter as  tk
 from tkinter.ttk import *
 import re
 import  os,sys
-import  asyncio
 
 class Page(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self,master=None)
 
-    async   def show(self):
-        self.lift()
-
     def page_set(self,url):
         self.response=requests.get(url)
         return BeautifulSoup(self.response.content,'html.parser')
+    
+    def close_window(self):
+        self.master.destroy()
 
 class   HomePage(Page,tk.Frame):
-    def __init__(self,master=None):
-        tk.Frame.__init__(self, master=None)
+    def __init__(self,master):
+        tk.Frame.__init__(self,master)
         self.soup=self.page_set("https://www.tabnews.com.br/")
         self.div_post=self.soup.find(class_='Box-sc-g0xbh4-0 kRPWSL')
         self.div_post_elements=self.div_post.find_all(class_="Box-sc-g0xbh4-0 fXxQUH")
@@ -38,27 +37,38 @@ class   HomePage(Page,tk.Frame):
                 self.button_post_page.pack()
                 
                 self.button_post_page.config(command=self.url_lambda_function)
+            
+            self.url_lambda_function()
         
     def url_lambda_function(self):
         self.url_lambda=lambda x=self.soup.find('a', string=self.post_title): self.get_lambda_url(x)
-        PostPage(root, url_string=self.url_lambda())
+        self.post_url=self.button_post_page.config(command=self.url_lambda)
+        self.buttons=[self.button_post_page]
+        self.button_id=str(self.buttons)
 
     def get_lambda_url(self,url):
         self.urls=[url]
         self.url_list=[link['href'] for link in self.urls]
         self.url_string=str(self.url_list).strip('[]').strip("'")
-        return  self.url_string
+        self.new_window=tk.Toplevel(self.master)
+        self.app=PostPage(self.new_window,)
 
 class PostPage(HomePage,tk.Frame,tk.Toplevel):
-    def __init__(self, master = None,url_string=None):
-        tk.Frame.__init__(self, master=None,)
-        self.soup = self.page_set(f"https://www.tabnews.com.br{url_string}")
-        self.post_page_window = tk.Toplevel(root)
+    def __init__(self, master,):
+        self.master=master
+        self.frame=tk.Frame(master)
+        master.title("a")
 
-        self.widget = tk.Frame(master)
-        self.widget.pack()
-        self.button_aa=tk.Button(self.widget, text="aaa").pack
+        self.quitButton = tk.Button(self.frame, text = 'Quit', width = 25, command = self.close_window)
+        self.quitButton.pack()
 
+        self.frame.pack()
+        
+        self.soup = self.page_set(f"https://www.tabnews.com.br{main.url_string}")
+
+        self.title=self.soup.find(class_="Heading__StyledHeading-sc-1c1dgg0-0 lcwJQJ").text.strip()        
+
+        self.button=tk.Button(self.frame, text="aaa").pack
 
 if __name__ == "__main__":
     os.system("clear")
